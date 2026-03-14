b88e9b66c16e7157921fc188266c5f3b250aeae2
cangu08dfd <robinsonquoc@yahoo.co.uk>
2026-03-15 05:02:12 +0700

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T22:02:00.152Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-14T22:02:10.632Z
+Commit: Fix broken links in documentation
