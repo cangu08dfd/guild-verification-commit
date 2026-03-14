e2c9d6926ff1916bdaa815cc6b1808b1f486d96e
cangu08dfd <robinsonquoc@yahoo.co.uk>
2026-03-15 05:01:45 +0700

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T22:01:40.184Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-14T22:01:44.772Z
+Commit: Fix broken links in documentation
