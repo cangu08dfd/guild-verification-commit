1aa2139ef5ee6d10bc9acc20eab1256b1dee9f42
cangu08dfd <robinsonquoc@yahoo.co.uk>
2026-03-15 07:31:25 +0700

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T22:02:14.998Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-15T00:31:24.234Z
+Commit: Fix broken links in documentation
